e65f674195ee4d58ca838dc899211db4986e02b0
Anton Semenikhin <protest_the_hero@mail.ru>
2023-01-29 16:43:28 +0300

    add express and socket.io

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-// получаем библитеку feathers
 const feathers = require('@feathersjs/feathers')
-
-// инициализируем feathers в переменной app
-const app = feathers()
+const express = require('@feathersjs/express')
+const socketio = require('@feathersjs/socketio')
 
 // создаем сервис сообщений
 class MessageService {
   constructor () {
-    // с массивом в котором будут храниться сообщения
+    // с массивом в котором будут храниться создаваемые сообщения
     this.messages = []
   }
 
-  // методом для получения сообщений
+  // метод для получения сообщений
   async find() {
     // который возвращает массив messages
     return this.messages
   }
 
-  // методом для создания нового сообщения и добавления его в массив сообщений messages
+  // метод для создания нового сообщения и добавления его в массив сообщений messages
   async create(data) {
     // модель сообщения
     const message = {
@@ -29,28 +27,38 @@ class MessageService {
   }
 }
 
+//Создает приложение Feathers, совместимое с ExpressJS.
+const app = express(feathers())
+
+//middleware
+// HTTP парсер
+app.use(express.json())
+// Парсинг для URL параметров
+app.use(express.urlencoded({ extended: true }))
+// Размещаем статические файлы из корня
+app.use(express.static(__dirname))
+// Подключаем поддержку rest api
+app.configure(express.rest())
+// Подключаем socket.io
+app.configure(socketio())
+
 // регестрируем сервис в app
-app.use('messages', new MessageService())
-// при вызове метода created внутри сервиса messages выводим в консоль сообщение
-app.service('messages').on('created', (message) => {
-  console.log('A new message was created', message);
+// json будет отображаться на роуте messages
+app.use('/messages', new MessageService())
+// Регистрируем обработчик ошибок
+app.use(express.errorHandler)
+
+// Создаем новый канал для всех
+app.on('connection', connection => {
+  app.chanel('everybody').join(connection)
+})
+// Отображаем все изменения для всех
+app.publish(() => app.channel('everybody'))
+
+app.listen(3030).on('listening', () => {
+  console.log('Feathers server listening on http://localhost:3030');
 })
 
-// метод main
-const main = async () => {
-  // вызываем метод create в котором передаем объект с текстом сообщения
-  await app.service('messages').create({
-    text: 'Hello Anton!'
-  })
-
-  await app.service('messages').create({
-    text: 'How are you?'
-  })
-
-  // в переменной messages получаем массив сообщений вызывая метод find в
-  const messages = await app.service('messages').find();
-  console.log('All my messages:', messages);
-}
-
-
-main()
+app.service('messages').create({
+  text: 'Hello, how is it going?'
+})
